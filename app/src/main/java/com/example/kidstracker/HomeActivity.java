b6f5ce3d6f44190c98fc99c9168ffa917f48aaee
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2020-11-01 19:23:38 -0800

    fix: Removed floating action button

--- app/src/main/java/com/example/kidstracker/HomeActivity.java
+++ app/src/main/java/com/example/kidstracker/HomeActivity.java
@@ -30,15 +30,7 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        //hello
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Will add a note section!", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
