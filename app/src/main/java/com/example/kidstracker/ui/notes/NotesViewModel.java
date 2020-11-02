bb11aa11bbfdf225b3548d222ea829d4e8fe3803
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2020-11-01 19:31:58 -0800

    feat: Added this file to allow insert, update, delete, and delete all methods for the repository

--- app/src/main/java/com/example/kidstracker/ui/notes/NotesViewModel.java
+++ app/src/main/java/com/example/kidstracker/ui/notes/NotesViewModel.java
@@ -1,18 +1,42 @@
 package com.example.kidstracker.ui.notes;
 
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
-public class NotesViewModel extends ViewModel {
-    private MutableLiveData<String> mText;
+import com.example.kidstracker.models.Note;
 
-    public NotesViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is my notes fragment");
+import java.util.List;
+
+public class NotesViewModel extends AndroidViewModel {
+
+    private NotesRepository mNotesRepository;
+    private LiveData<List<Note>> allNotes;
+
+    public NotesViewModel(Application application) {
+        super(application);
+        mNotesRepository = new NotesRepository(application);
+        allNotes = mNotesRepository.getAllNotes();
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public void insert(Note note) {
+        mNotesRepository.insert(note);
+    }
+
+    public void update(Note note) {
+        mNotesRepository.update(note);
+    }
+
+    public void delete(Note note) {
+        mNotesRepository.delete(note);
+    }
+
+    public void deleteAllNotes() {
+        mNotesRepository.deleteAllNotes();
+    }
+
+    public LiveData<List<Note>> getAllNotes() {
+        return allNotes;
     }
 }
