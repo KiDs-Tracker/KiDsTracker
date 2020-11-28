8f2f52e7d8082fc5948eb6ceab116990904c9571
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2020-11-27 20:34:47 -0800

    fix: removed fields for this class

--- app/src/main/java/com/example/kidstracker/models/MedicalQuestion.java
+++ app/src/main/java/com/example/kidstracker/models/MedicalQuestion.java
@@ -13,17 +13,8 @@ public class MedicalQuestion implements Serializable {
 
     private String question;
 
-    private String option1;
-
-    private String option2;
-
-    private String option3;
-
-    public MedicalQuestion(String question, String option1, String option2, String option3) {
+    public MedicalQuestion(String question) {
         this.question = question;
-        this.option1 = option1;
-        this.option2 = option2;
-        this.option3 = option3;
     }
 
     public int getId() {
@@ -42,27 +33,4 @@ public class MedicalQuestion implements Serializable {
         this.question = question;
     }
 
-    public String getOption1() {
-        return option1;
-    }
-
-    public void setOption1(String option1) {
-        this.option1 = option1;
-    }
-
-    public String getOption2() {
-        return option2;
-    }
-
-    public void setOption2(String option2) {
-        this.option2 = option2;
-    }
-
-    public String getOption3() {
-        return option3;
-    }
-
-    public void setOption3(String option3) {
-        this.option3 = option3;
-    }
 }
