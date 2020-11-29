c57dacc4586ce45dce3acb9f779496a34b9f8012
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2020-11-29 13:10:01 -0800

    feat: added a shared preferences and alert dialog

--- app/src/main/java/com/example/kidstracker/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/kidstracker/ui/settings/SettingsFragment.java
@@ -1,6 +1,12 @@
 package com.example.kidstracker.ui.settings;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,22 +14,80 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
+import com.example.kidstracker.MainActivity;
 import com.example.kidstracker.R;
+import com.example.kidstracker.databinding.FragmentSettingsBinding;
 
 public class SettingsFragment extends Fragment {
 
+    public static final String TAG = SettingsFragment.class.getSimpleName();
+
     private SettingsViewModel mSettingsViewModel;
+    private FragmentSettingsBinding mBinding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        mSettingsViewModel =
-                new ViewModelProvider(this).get(SettingsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_settings, container, false);
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false);
+        View root = mBinding.getRoot();
+
+        setUsername();
+        signOutUser();
+        goToInfo();
 
         return root;
+    }
+
+    private void goToInfo() {
+        mBinding.layoutInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(getView()).navigate(R.id.nav_about);
+            }
+        });
+    }
+
+    private void signOutUser() {
+        mBinding.tvSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(getContext())
+                        .setTitle("Sign Out")
+                        .setMessage("Are you sure you would like to sign out?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putBoolean("checkbox", false);
+                                editor.apply();
+                                startActivity(new Intent(getActivity(), MainActivity.class));
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        }).create().show();
+            }
+        });
+    }
+
+    private void setUsername() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        String userName = sharedPreferences.getString("userName", "");
+        mBinding.tvUsername.setText(userName);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mSettingsViewModel = new ViewModelProvider(this).get(SettingsViewModel.class);
     }
 }
