a2974fa11c94c388d9edf422cc65c49d6fa1e9ad
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2020-11-30 15:19:03 -0800

    feat: added a fragment for add provider

--- app/src/main/java/com/example/kidstracker/ui/healthcare/addprovider/AddProviderFragment.java
+++ app/src/main/java/com/example/kidstracker/ui/healthcare/addprovider/AddProviderFragment.java
@@ -1,6 +1,5 @@
 package com.example.kidstracker.ui.healthcare.addprovider;
 
-import androidx.annotation.NavigationRes;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -13,7 +12,6 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
-import android.renderscript.ScriptGroup;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,6 +28,7 @@ public class AddProviderFragment extends Fragment {
     private AddFragmentBinding mBinding;
     private AddViewModel mViewModel;
     private Provider mProvider = new Provider();
+    private Provider provider;
 
 
 
@@ -43,8 +42,79 @@ public class AddProviderFragment extends Fragment {
         onProviderTypeClick();
         onRegisterProviderClick();
 
+        if (!(getArguments() == null)) {
+            setAddEditProviders();
+        }
 
         return root;
+    }
+
+    private void setAddEditProviders() {
+        provider = (Provider) getArguments().getSerializable("provider");
+        mBinding.etFacilityLocation.setText(provider.getLocation());
+        mBinding.etPatientName.setText(provider.getPatientName());
+        mBinding.etProviderName.setText(provider.getName());
+        mBinding.etProviderNumber.setText(provider.getNumber());
+
+        String occupation = "";
+        switch(provider.getPhysician()){
+            case 0:
+                occupation = "Cardiologist";
+                mBinding.tvProvider.setText(occupation);
+                break;
+            case 1:
+                occupation = "Neurologist";
+                mBinding.tvProvider.setText(occupation);
+                break;
+            case 2:
+                occupation = "Dentist";
+                mBinding.tvProvider.setText(occupation);
+                break;
+            case 3:
+                occupation = "Optometrist";
+                mBinding.tvProvider.setText(occupation);
+                break;
+            case 4:
+                occupation = "Ophthalmologist";
+                mBinding.tvProvider.setText(occupation);
+                break;
+            case 5:
+                occupation = "Otolaryngologist";
+                mBinding.tvProvider.setText(occupation);
+                break;
+            case 6:
+                occupation = "Pulmonologist";
+                mBinding.tvProvider.setText(occupation);
+                break;
+            case 7:
+                occupation = "Surgeon";
+                mBinding.tvProvider.setText(occupation);
+                break;
+            case 8:
+                occupation = "Primary Care Doctor";
+                mBinding.tvProvider.setText(occupation);
+                break;
+            case 9:
+                occupation = "Orthopedist";
+                mBinding.tvProvider.setText(occupation);
+                break;
+            case 10:
+                occupation = "Gastroenterologist";
+                mBinding.tvProvider.setText(occupation);
+                break;
+        }
+
+        String gender = "";
+        switch(provider.getGender()){
+            case 0:
+                gender = "Male";
+                mBinding.tvGender.setText(gender);
+                break;
+            case 1:
+                gender = "Female";
+                mBinding.tvGender.setText(gender);
+                break;
+        }
     }
 
     private void onRegisterProviderClick() {
@@ -59,15 +129,26 @@ public class AddProviderFragment extends Fragment {
                 if(providerName.isEmpty() || patientName.isEmpty() || number.isEmpty() || location.isEmpty()){
                     Toasty.error(getActivity(),"Please fill out all entries!", Toast.LENGTH_SHORT, true).show();
                 }else {
-                    mProvider.setName(providerName);
-                    mProvider.setPatientName(patientName);
-                    mProvider.setNumber(number);
-                    mProvider.setLocation(location);
-                    mViewModel.insertProvider(mProvider);
+
+                    if (!(getArguments() == null)) {
+                        Provider updatedProvider = new Provider();
+                        updatedProvider.setId(provider.getId());
+                        updatedProvider.setGender(provider.getGender());
+                        updatedProvider.setPhysician(provider.getPhysician());
+                        updatedProvider.setLocation(location);
+                        updatedProvider.setName(providerName);
+                        updatedProvider.setNumber(number);
+                        updatedProvider.setPatientName(patientName);
+                        mViewModel.updateProvider(updatedProvider);
+                    } else {
+                        mProvider.setName(providerName);
+                        mProvider.setPatientName(patientName);
+                        mProvider.setNumber(number);
+                        mProvider.setLocation(location);
+                        mViewModel.insertProvider(mProvider);
+                    }
 
                     goToHealthCare();
-
-
                 }
 
             }
@@ -92,57 +173,101 @@ public class AddProviderFragment extends Fragment {
                                     case 0:
                                         physician = "Cardiologist";
                                         mBinding.tvProvider.setText(physician);
-                                        mProvider.setPhysician(0);
+                                        if (!(getArguments() == null)) {
+                                            provider.setPhysician(0);
+                                        } else {
+                                            mProvider.setPhysician(0);
+                                        }
                                         break;
                                     case 1:
                                         physician = "Neurologist";
                                         mBinding.tvProvider.setText(physician);
-                                        mProvider.setPhysician(1);
+                                        if (!(getArguments() == null)) {
+                                            provider.setPhysician(1);
+                                        } else {
+                                            mProvider.setPhysician(1);
+                                        }
                                         break;
                                     case 2:
                                         physician = "Dentist";
                                         mBinding.tvProvider.setText(physician);
-                                        mProvider.setPhysician(2);
+                                        if (!(getArguments() == null)) {
+                                            provider.setPhysician(2);
+                                        } else {
+                                            mProvider.setPhysician(2);
+                                        }
                                         break;
                                     case 3:
                                         physician = "Optometrist";
                                         mBinding.tvProvider.setText(physician);
-                                        mProvider.setPhysician(3);
+                                        if (!(getArguments() == null)) {
+                                            provider.setPhysician(3);
+                                        } else {
+                                            mProvider.setPhysician(3);
+                                        }
                                         break;
                                     case 4:
                                         physician = "Ophthalmologist";
                                         mBinding.tvProvider.setText(physician);
-                                        mProvider.setPhysician(4);
+                                        if (!(getArguments() == null)) {
+                                            provider.setPhysician(4);
+                                        } else {
+                                            mProvider.setPhysician(4);
+                                        }
                                         break;
                                     case 5:
                                         physician = "Otolaryngologist";
                                         mBinding.tvProvider.setText(physician);
-                                        mProvider.setPhysician(5);
+                                        if (!(getArguments() == null)) {
+                                            provider.setPhysician(5);
+                                        } else {
+                                            mProvider.setPhysician(5);
+                                        }
                                         break;
                                     case 6:
                                         physician = "Pulmonologist";
                                         mBinding.tvProvider.setText(physician);
-                                        mProvider.setPhysician(6);
+                                        if (!(getArguments() == null)) {
+                                            provider.setPhysician(6);
+                                        } else {
+                                            mProvider.setPhysician(6);
+                                        }
                                         break;
                                     case 7:
                                         physician = "Surgeon";
                                         mBinding.tvProvider.setText(physician);
-                                        mProvider.setPhysician(7);
+                                        if (!(getArguments() == null)) {
+                                            provider.setPhysician(7);
+                                        } else {
+                                            mProvider.setPhysician(7);
+                                        }
                                         break;
                                     case 8:
                                         physician = "Primary Care Doctor";
                                         mBinding.tvProvider.setText(physician);
-                                        mProvider.setPhysician(8);
+                                        if (!(getArguments() == null)) {
+                                            provider.setPhysician(8);
+                                        } else {
+                                            mProvider.setPhysician(8);
+                                        }
                                         break;
                                     case 9:
                                         physician = "Orthopedist";
                                         mBinding.tvProvider.setText(physician);
-                                        mProvider.setPhysician(9);
+                                        if (!(getArguments() == null)) {
+                                            provider.setPhysician(9);
+                                        } else {
+                                            mProvider.setPhysician(9);
+                                        }
                                         break;
                                     case 10:
                                         physician = "Gastroenterologist";
                                         mBinding.tvProvider.setText(physician);
-                                        mProvider.setPhysician(10);
+                                        if (!(getArguments() == null)) {
+                                            provider.setPhysician(10);
+                                        } else {
+                                            mProvider.setPhysician(10);
+                                        }
                                         break;
 
 
@@ -170,13 +295,21 @@ public class AddProviderFragment extends Fragment {
                                     case 0:
                                         gender = "Male";
                                         mBinding.tvGender.setText(gender);
-                                        mProvider.setGender(0);
+                                        if (!(getArguments() == null)) {
+                                            provider.setGender(0);
+                                        } else {
+                                            mProvider.setGender(0);
+                                        }
                                         break;
 
                                     case 1:
                                         gender = "Female";
                                         mBinding.tvGender.setText(gender);
-                                        mProvider.setGender(1);
+                                        if (!(getArguments() == null)) {
+                                            provider.setGender(1);
+                                        } else {
+                                            mProvider.setGender(1);
+                                        }
                                         break;
                                 }
                             }
